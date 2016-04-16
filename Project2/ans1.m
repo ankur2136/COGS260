236d7ffd3eb572a2851c6b88cae8c3ec9095d073
Ankur Jain <ankur.jain@nsitonline.in>
2016-04-16 13:03:07 -0700

    Adding project2

---
+++ Project2/ans1.m
@@ -0,0 +1,36 @@
+%%
+train_images = loadMNISTImages('train-images-idx3-ubyte');
+train_labels = loadMNISTLabels('train-labels-idx1-ubyte');
+
+test_images = loadMNISTImages('t10k-images-idx3-ubyte');
+test_labels = loadMNISTLabels('t10k-labels-idx1-ubyte');
+
+%%
+tic
+confusion_mat = zeros(10,10);
+index_wrong = -1*ones(10000, 1);
+number_correct = 0;
+for i=1:1000
+    dist = 100000;
+    chosen_value = -1;
+    test = test_images(:,i);
+    for j=1:60000
+        train = train_images(:,j);
+        distance_new = norm(train-test);
+        
+        if (distance_new < dist)
+            dist = distance_new;
+            chosen_value = train_labels(j);
+        end
+    end
+    
+    if (chosen_value == test_labels(i)) 
+        number_correct = number_correct +1;
+    else 
+        confusion_mat(test_labels(i)+1, chosen_value+1) = ...
+                   confusion_mat(test_labels(i)+1, chosen_value+1) + 1;     
+        index_wrong(i)=chosen_value;
+    end
+end
+toc
+disp (number_correct);
