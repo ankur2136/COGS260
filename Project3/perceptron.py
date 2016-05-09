8bd57c166cf0063be9573b20b8103ebd3d6d1f1b
Ankur Jain <ankur.jain@nsitonline.in>
2016-05-09 09:22:45 -0700

    Completed 1,4,5,6 part of Q3

--- Project3/perceptron.py
+++ Project3/perceptron.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def dot_product(a, b):
     return sum([a[i]*b[i] for i in range(len(a))])
 
@@ -8,60 +10,101 @@ def decision( x, w, theta ):
 
 def perceptron( training_data ):
     theta = 0
-    weights = [ 0 ]
-    x = 0
+    iteration = 0
+    weights = [ 0, 0, 0, 0 ]
     converged = False
     
     while not converged:
-        x += 1
-        if x > 50: # Being lazy for now
-            converged = True
-
+	correct_count = 0;
         for key, val in training_data.iteritems():
             d = decision(key, weights, theta)
             if d == val:
+		correct_count +=1 
                 continue
             elif d == False and val == True:
                 theta -= 1
+		iteration += 1
                 for i in range(len(key)):
                     weights[i] += key[i]
 
             elif d == True  and val == False:
                 theta += 1
+                iteration += 1
                 for i in range(len(key)):
                     weights[i] -= key[i]
-        print weights, theta
 
+        if (correct_count == len(training_data)):
+	    break;
+
+    print ("Converged in Iterations {}".format(iteration))	
     return weights, theta
 
+lines = [line.rstrip('\n') for line in open('iris/iris_train.data')]
 
-training_data = {
-        # Tuples!
-        (  1, ) : True,
-        ( 15, ) : False,
-        ( 25, ) : False,
-        (  3, ) : True,
-        (  9, ) : True,
-        ( 55, ) : False,
-        (  2, ) : True,
-}
+train = {}
+for line in lines:
+    words = line.split(',')
+    tup   = (float(words[0]),float(words[1]),float(words[2]),float(words[3]),) 
+    if (words[4] == 'Iris-setosa'):
+	train[tup] = True
+    else: 
+        train[tup] = False
 
-weights, theta = perceptron( training_data )
+lines1 = [line.rstrip('\n') for line in open('iris/iris_test.data')]
 
-print "Done!"
+test = {}
+for line in lines1:
+    words = line.split(',')
+    tup   = (float(words[0]),float(words[1]),float(words[2]),float(words[3]),)
+    if (words[4] == 'Iris-setosa'):
+        test[tup] = True
+    else:
+        test[tup] = False
 
-tests = {
-        ( 30, ) : False,
-        ( 20, ) : False,
-        (  2, ) : True,
-        (  8, ) : True,
-        ( 15, ) : False
-}
+#print (train)
+#print (test)
 
-for key, val in tests.iteritems():
+
+train_feat = np.asarray(train.keys())
+test_feat = np.asarray(test.keys())
+
+train_mean = np.mean(train_feat, axis=0)
+test_mean = np.mean(test_feat, axis=0)
+train_std = np.std(train_feat, axis=0)
+test_std = np.std(test_feat, axis=0)
+
+train_z = {}
+for key, val in train.iteritems():
+	key_new = tuple(np.array((key-train_mean)/train_std))
+	train_z[key_new] = val
+
+test_z = {}
+for key, val in test.iteritems():
+        key_new = tuple(np.array((key-train_mean)/train_std))
+        test_z[key_new] = val
+	
+
+
+weights, theta = perceptron( train )
+total_correct = 0
+for key, val in test.iteritems():
     d = decision(key, weights, theta)
-    answer = "WRONG"
     if d == val:
-        answer = "Correct!"
-    print answer, key, d, val
+	total_correct += 1
+    
+print ("No Z scoring\n")
+print ("Total Correct = {}, out of {}".format(total_correct, len(test))) 
+
+
+
+weights1, theta1 = perceptron( train_z )
+total_correct = 0
+for key, val in test_z.iteritems():
+    d = decision(key, weights1, theta1)
+    if d == val:
+        total_correct += 1
+   
+print ("With Z scoring\n")
+print ("Total Correct = {}, out of {}".format(total_correct, len(test_z)))
+
 
